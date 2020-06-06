a1ba0fbe92faf7cdb8accff15b05ee9ae744bd58
Maks Nemisj <nemisj@nemisj.com>
2020-06-06 12:12:16 +0200

    card component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Card from './Card.svelte';
 	import { existingWords } from './words.js';
 	import { onMount } from 'svelte';
 
@@ -118,17 +119,7 @@
 
 <main>
 	{#if current}
-	<div class="scene">
-		<div class="zork {flipped && 'flipped'}">
-			<div class="text" style="visibility: hidden">{current[0].length > current[1].length ? current[0] : current[1]}</div>
-			<h1 class="card text back">{current[0]}</h1>
-			<h1 class="card text front">{current[1]}</h1>
-			<div class="buttons-container">
-				<div class="good" on:click="{() => next(true)}" />
-				<div class="bad" on:click="{() => next(false)}" />
-			</div>
-		</div>
-	</div>
+		<Card back={current[0]} front={current[1]} flipped={flipped} onClick={next} />
 	{/if}
 	{#if !current}
 		There are no words anymore. Genius;
@@ -137,91 +128,10 @@
 
 <style>
 
-	.scene {
-		perspective: 600px;
-	}
-
-	.buttons-container {
-		position: absolute;
-		left: 0;
-		top: 0;
-		width: 100%;
-		height: 100%;
-		cursor: pointer;
-		display: flex;
-	}
-
-	.buttons-container > div {
-		flex-grow: 1;
-	}
-
-	.good {
-		cursor: w-resize;
-	}
-
-	.bad {
-		cursor: e-resize;
-	}
-
-	.zork {
-		display: inline-block;
-		position: relative;
-		perspective: 600px;
-		transform-style: preserve-3d;
-		transition: transform 2s;
-	}
-
-	.zork.flipped {
-		transform: rotateY( 180deg );
-	}
-
-	.zork.flipped .buttons-container {
-		transform: rotateY(180deg);
-		cursor: inherit;
-	}
-
 	main {
 		text-align: center;
 		padding: 1em;
 		margin: 0 auto;
-	}
-
-	.card {
-		position: relative;
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		left: 0;
-		backface-visibility: hidden;
-		background-color: #f4f4f4;
-		border-radius: 10px;
-	}
-
-	.text {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-		margin: 0;
-		padding: 1rem 2rem;
-		box-sizing: border-box;
-		justify-content: center;
-		display: flex;
-		align-items: center;
-	}
-
-	.front {
-	}
-
-	.back {
-		color: transparent;
-		background: #ff3e00;
-		transform: rotateY( 180deg );
-	}
-
-	.flipped .back {
-		color: #fff;
 	}
 
 </style>
---
+++ src/Card.svelte
@@ -0,0 +1,98 @@
+<script>
+	export let back;
+	export let front;
+	export let onClick;
+	export let flipped;
+</script>
+
+<div class="scene">
+	<div class="zork {flipped && 'flipped'}">
+		<div class="text" style="visibility: hidden">{back.length > front.length ? back : front}</div>
+		<h1 class="card text back">{back}</h1>
+		<h1 class="card text">{front}</h1>
+		<div class="buttons-container">
+			<div class="good" on:click={() => onClick(true)} />
+			<div class="bad" on:click={() => onClick(false)} />
+		</div>
+	</div>
+</div>
+
+<style>
+	.scene {
+		perspective: 600px;
+	}
+
+	.zork {
+		display: inline-block;
+		position: relative;
+		perspective: 600px;
+		transform-style: preserve-3d;
+		transition: transform 2s;
+	}
+
+	.zork.flipped {
+		transform: rotateY( 180deg );
+	}
+
+	.text {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+		margin: 0;
+		padding: 1rem 2rem;
+		box-sizing: border-box;
+		justify-content: center;
+		display: flex;
+		align-items: center;
+	}
+
+	.card {
+		position: relative;
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		backface-visibility: hidden;
+		background-color: #f4f4f4;
+		border-radius: 10px;
+	}
+
+	.back {
+		color: transparent;
+		background: #ff3e00;
+		transform: rotateY( 180deg );
+	}
+
+	.flipped .back {
+		color: #fff;
+	}
+
+	.buttons-container {
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		cursor: pointer;
+		display: flex;
+	}
+
+	.buttons-container > div {
+		flex-grow: 1;
+	}
+
+	.good {
+		cursor: w-resize;
+	}
+
+	.bad {
+		cursor: e-resize;
+	}
+
+	.flipped .buttons-container {
+		transform: rotateY(180deg);
+		cursor: inherit;
+	}
+</style>
